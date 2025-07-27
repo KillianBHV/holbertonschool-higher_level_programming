7ae6aec91a70e515930ee5cdb8f6be27166c13ec
Killian BEHAVA <behavakillian70@gmail.com>
2025-07-27 23:25:25 +0200

    Get number of occurences of a score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- GET NUMBER OF SCORES
-SELECT DISTINCT score, COUNT(score) AS number FROM second_table ORDER BY score DESC;
+SELECT DISTINCT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
