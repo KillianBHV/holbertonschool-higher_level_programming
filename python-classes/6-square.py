ed6dc3e80bdbeaec0a67881410caaa25e7c8ad36
Killian BEHAVA <behavakillian70@gmail.com>
2025-07-28 14:28:17 +0200

    my print improvment

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -58,13 +58,7 @@ class Square:
             return
 
         for i in range(self.__position[1]):
-            print("")
+            print()
 
         for i in range(self.__size):
-            for j in range(self.__position[0]):
-                print(" ", end="")
-
-            for k in range(self.__size):
-                print("#", end="")
-
-        print("")
+            print(self.__position[0] * " " + self.__size * "#")
