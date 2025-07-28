11867f12f61894877fd2c9e595143ad58229c2fc
Killian BEHAVA <behavakillian70@gmail.com>
2025-07-28 10:12:31 +0200

    Get private attribute __size

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Create a square"""
+
+
+class Square:
+    """Defining the attributes of a square"""
+    def __init__(self, size=0):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """This is a setter for the private attribute __size"""
+    def size(self, value):
+        if not value is int:
+            raise TypeError("size must be an integer")
+        elif if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            return self.__size
+
+    """Get the area of a square"""
+    def area(self):
+        return self.__size * self.__size
