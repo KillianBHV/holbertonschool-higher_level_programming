aafe1a22736461013a426d8ff1c543f1d454d0aa
Killian BEHAVA <behavakillian70@gmail.com>
2025-07-28 10:05:50 +0200

    Get the area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Create a square"""
+
+
+class Square:
+    """Defining the attributes of a square"""
+    def __init__(self, size=0):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+
+    """Get the area of a square"""
+    def area(self):
+        return self.__size * self.__size
