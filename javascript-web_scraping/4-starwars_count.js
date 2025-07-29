033d7c88eb13e72b950633c98881b40bbdfe8d36
Killian BEHAVA <behavakillian70@gmail.com>
2025-07-29 14:56:12 +0200

    Count the movies

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], (e, r, b) => {
+  if(!e) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      if(movie.characters.find(c => c.endsWith('/18/')))
+        return count + 1;
+      return count;
+    }));
+  }
+});
