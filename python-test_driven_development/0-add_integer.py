60d02db53e0f6366408c19568b220a1e14811699
Killian BEHAVA <behavakillian70@gmail.com>
2025-07-29 09:48:31 +0200

    Add two integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,7 +3,7 @@
 
 
 def add_integer(a, b=98):
-"""Add two integers a and b"""
+    """Add two integers a and b"""
     if not type(a) is int and not type(a) is float:
         raise TypeError("a must be an integer")
     elif not type(b) is int and not type(b) is float:
